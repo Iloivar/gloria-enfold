968382c4391725c70c534e520b10ef787936a559
Iloivar <riccardo.meinert@addmore.de>
2020-04-23 22:49:02 +0200

    startpage zwischenstand

--- wp-content/themes/enfold-child/functions.php
+++ wp-content/themes/enfold-child/functions.php
@@ -4,3 +4,21 @@
 * Add your own functions here. You can also copy some of the theme functions into this file. 
 * Wordpress will use those functions instead of the original functions then.
 */
+
+add_filter('avia_load_shortcodes', 'avia_include_shortcode_template', 15, 1);
+function avia_include_shortcode_template($paths)
+{
+    $template_url = get_stylesheet_directory();
+    array_unshift($paths, $template_url.'/shortcodes/');
+
+    return $paths;
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/css/custom.css',
+        array('parent-style')
+    );
+}
